e37cc54b3734f733fa16a833f1fe080453407d8c
Luke Taylor <luke@circleleasing.co.uk>
2023-12-02 21:39:03 +0000

    2.1

---
+++ 2/1.js
@@ -0,0 +1,60 @@
+const { getPuzzleInput } = require("../utils");
+
+const limits = {
+    red: 12,
+    green: 13,
+    blue: 14
+};
+
+function parseReveal(reveal) {
+    const parts = reveal.split(",");
+    const parsed = {
+        red: 0,
+        green: 0,
+        blue: 0
+    };
+
+    parts.forEach(p => {
+        p = p.trim();
+        const [amount, colour] = p.split(" ");
+        parsed[colour] = parseInt(amount);
+    });
+
+    return parsed;
+}
+
+function parseInput(line) {
+    const [idString, data] = line.split(":");
+    const id = parseInt(idString.split(" ")[1]);
+
+    const maxGuess = {
+        red: 0,
+        green: 0,
+        blue: 0
+    };
+
+    const reveals = data.split(";");
+    for (const r of reveals) {
+        const reveal = parseReveal(r);
+        Object.keys(reveal).forEach(k => maxGuess[k] = Math.max(maxGuess[k], reveal[k]));
+    }
+
+    return {
+        id,
+        maxGuess
+    };
+}
+
+function getInputValue(line){
+    const parsedInput = parseInput(line);
+    for(const k of Object.keys(parsedInput.maxGuess)){
+        if(parsedInput.maxGuess[k] > limits[k]){
+            return 0;
+        }
+    }
+
+    return parsedInput.id;
+}
+
+const score = getPuzzleInput(__dirname).reduce((acc, val) => acc + getInputValue(val), 0);
+console.log(score);
