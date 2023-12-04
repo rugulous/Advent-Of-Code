ab1850a8a5e7b83376c57027826bf089ef1d3b1b
Luke Taylor <luke@circleleasing.co.uk>
2023-12-04 23:05:09 +0000

    Split was still leaving in \r

--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 function getPuzzleInput(dir, file = "input.txt"){
     const input = fs.readFileSync(path.join(dir, file), "utf-8");
-    return input.split("\n");
+    return input.split("\r\n");
 }
 
 module.exports = {
