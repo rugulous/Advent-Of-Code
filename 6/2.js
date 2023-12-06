315d6ffdb43bae51497c0b53b61b7d344b9f01b6
Luke Taylor <luke@circleleasing.co.uk>
2023-12-06 10:09:52 +0000

    6.2E

---
+++ 6/2.js
@@ -0,0 +1,43 @@
+const {getPuzzleInput} = require("../utils");
+
+function extractNumbers(line){
+    return [parseInt(line.split(":")[1].replaceAll(" ", ""))];
+}
+
+function translateInput(){
+    const time = extractNumbers(input[0])
+    const distance = extractNumbers(input[1]);
+    const races = [];
+    
+    for(let i = 0; i < time.length; i++){
+        races.push({
+            time: time[i],
+            distance: distance[i]
+        });
+    }
+
+    return races;
+}
+
+function getMultiples(target, limit){
+    let multiples = 0;
+
+    for(let i = 1; i < limit; i++){
+        const attempt = limit - i;
+        const result = i * attempt;
+
+        if(result > target){
+            console.log(`${i} x ${attempt} = ${result} (target ${target})`);
+            multiples++;
+        }
+    }
+
+    console.log(multiples);
+    return multiples;
+}
+
+const input = getPuzzleInput(__dirname, "example.txt");
+const races = translateInput();
+
+const result = races.reduce((acc, r) => acc * getMultiples(r.distance, r.time), 1);
+console.log(result);
