e4e33719d7d88f919fc4223960625a0ca60d1f9b
Luke Taylor <luke@circleleasing.co.uk>
2023-12-06 10:22:06 +0000

    6.2

--- 6/2.js
+++ 6/2.js
@@ -28,7 +28,7 @@ function getMultiples(target, limit){
 
         if(result > target){
             console.log(`${i} x ${attempt} = ${result} (target ${target})`);
-            multiples++;
+            return (limit + 1) - (i * 2);
         }
     }
 
@@ -36,7 +36,7 @@ function getMultiples(target, limit){
     return multiples;
 }
 
-const input = getPuzzleInput(__dirname, "example.txt");
+const input = getPuzzleInput(__dirname);
 const races = translateInput();
 
 const result = races.reduce((acc, r) => acc * getMultiples(r.distance, r.time), 1);
