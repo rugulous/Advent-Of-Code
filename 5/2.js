f5d7124c10bddfbbc535fd252a7ff723cb2e9622
Luke Taylor <luke@circleleasing.co.uk>
2023-12-06 10:51:38 +0000

    5.2E?

---
+++ 5/2.js
@@ -0,0 +1,124 @@
+const { getPuzzleInput } = require("../utils");
+
+let currMap = null;
+let allMap = {};
+
+const order = ['seed', 'soil', 'fertilizer', 'water', 'light', 'temperature', 'humidity'].reverse();
+
+const sortedKeys = {};
+let maxSize = -1;
+
+function expandSeeds(line){
+    const seeds = [];
+    const segments = line.split(":")[1].trim().split(" ").map(s => parseInt(s));
+
+    for(let i = 0; i < segments.length; i += 2){
+        for(let s = segments[i]; s < segments[i] + segments[i + 1]; s++){
+            seeds.push(s);
+        }
+    }
+
+    return seeds;
+}
+
+function processNewMap(line) {
+    if(currMap != null){
+        sortedKeys[currMap] = Object.keys(allMap[currMap]).map(v => parseInt(v)).sort((a,b) => a - b);
+    }
+
+    currMap = line.split(" ")[0].split("-")[0].trim();
+    allMap[currMap] = {};
+}
+
+function processLine(line) {
+    if(line == ""){
+        return;
+    }
+
+    if (line.indexOf("map") > 0) {
+        processNewMap(line);
+        return;
+    }
+
+    const parts = line.split(" ").map(num => parseInt(num));
+    buildMap(parts);
+}
+
+function buildMap(parts){
+    const newMax = Math.max(parts[0], parts[1]) + parts[2];
+    if(newMax > maxSize){
+        maxSize = newMax;
+    }
+
+    allMap[currMap][parts[0]] = [parts[1], parts[2]];
+}
+
+function tryGetMapValue(map, val){
+    console.log(`Mapping ${map} ${val}...`);
+
+    if(!allMap[map].hasOwnProperty(val)){
+        console.log("Not in map!");
+        let closestKey = null;
+        for(const key of sortedKeys[map]){
+            if(key > val){
+                break;
+            }
+
+            closestKey = key;
+        }
+
+        if(closestKey === null){
+            console.log("No closest key");
+            return val;
+        }
+
+        const relevantEntry = allMap[map][closestKey];
+        console.log(`Closest key: ${closestKey}`);
+        console.log(`Distance from key: ${val} - ${closestKey} = ${val - closestKey}`);
+        console.log(relevantEntry);
+
+        if(relevantEntry && (val >= closestKey && val <= closestKey + relevantEntry[1])){
+            console.log(`${val} in bounds`);
+            return (val - closestKey) + relevantEntry[0];
+        }
+
+        console.log(`${val} out of bounds`);
+        return val;
+    }
+
+    return allMap[map][val][0];
+}
+
+function getSeedAtLocation(location){
+    console.log(`Starting location ${location}`);
+    let index = location;
+
+    for(const key of order){
+        index = tryGetMapValue(key, index);
+        console.log();
+        console.log(`New index: ${index}`);
+    }
+
+    console.log(`Final value: ${index}`);
+    if(seeds.includes(index)){
+        console.log(`${index} is a seed!`);
+        return location;
+    }
+
+    return null;
+}
+
+const input = getPuzzleInput(__dirname, "example.txt");
+
+input.slice(1).forEach(processLine);
+processNewMap(""); //make sure we sort the final map too!
+
+const seeds = expandSeeds(input[0]);
+
+const locations = [];
+for(let i = 0; i < maxSize; i++){
+    locations.push(getSeedAtLocation(i));
+}
+
+console.log(`Finished for max size of ${maxSize}`);
+console.log(Math.min(...locations.filter(x => x)));
