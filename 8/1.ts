46b80e168cea336370f3203be5a342cf8d2c5352
Luke Taylor <luke@circleleasing.co.uk>
2023-12-08 09:27:10 +0000

    8.1E

---
+++ 8/1.ts
@@ -0,0 +1,31 @@
+import { ILooseObject } from '../type';
+import {getPuzzleInput} from '../utils';
+
+const nodes: ILooseObject = {};
+
+function parseInput(line: string){
+    const parts = line.split(" = ");
+    const linkedNodes = parts[1].replaceAll("(", "").replaceAll(")", "").replaceAll(" ", "").split(",");
+    nodes[parts[0].trim()] = linkedNodes;
+}
+
+function solve(){
+    let currentNode = "AAA";
+    let currMoves = 0;
+
+    while(currentNode != "ZZZ"){
+        for(const direction of directions){
+            currentNode = nodes[currentNode][direction];
+            currMoves++;
+        }
+    }
+
+    return currMoves;
+}
+
+const input = getPuzzleInput(__dirname, "example-2.txt");
+const directions = input[0].split("").map(d => d == "L" ? 0 : 1);
+input.splice(2).forEach(l => parseInput(l));
+
+const requiredMoves = solve();
+console.log(requiredMoves);
