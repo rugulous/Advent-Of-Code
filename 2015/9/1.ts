a768028d40232e2529388705c1c500aff6b4246a
Luke Taylor <luke@circleleasing.co.uk>
2023-12-08 20:55:36 +0000

    15/9.1E

---
+++ 2015/9/1.ts
@@ -0,0 +1,59 @@
+import { ILooseObject } from "../../type";
+import { getPuzzleInput } from "../../utils";
+
+const map: ILooseObject = {};
+
+function getShortestNotVisited(currNode: string, visited: string[]){
+    let lowest = null;
+
+    for(const key of Object.keys(map[currNode])){
+        if(visited.includes(key)){
+            continue;
+        }
+
+        if(lowest == null || map[currNode][key] < lowest.distance){
+            lowest = {
+                label: key,
+                distance: map[currNode][key]
+            }
+        }
+    }
+
+    return lowest;
+}
+
+function getRoute(start: string){
+    let currNode = start;
+    const visited = [];
+    let distance = 0;
+
+    while(true){
+        const closest = getShortestNotVisited(currNode, visited);
+        if(closest == null){
+            //hopefully we're done lol
+            return distance;
+        }
+
+        distance += closest.distance;
+        visited.push(closest.label);
+    }
+}
+
+function parseLine(line: string){
+    const [start, , end, , distanceStr] = line.split(" ");
+
+    if(!map.hasOwnProperty(start)){
+        map[start] = {};
+    }
+
+    if(!map.hasOwnProperty(end)){
+        map[end] = {};
+    }
+
+    const distance = parseInt(distanceStr);
+    map[start][end] = distance;
+    map[end][start] = distance;
+}
+
+getPuzzleInput(__dirname, "example.txt").forEach(parseLine);
+console.log(Math.min(...Object.keys(map).map(getRoute)));
