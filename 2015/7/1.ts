e24c52177c747b51a09d600b12b4265d35570ec8
Luke Taylor <luke@circleleasing.co.uk>
2023-12-08 15:52:56 +0000

    15/7.1E

--- 2015/7/1.ts
+++ 2015/7/1.ts
@@ -1,20 +1,103 @@
 import { ILooseObject } from "../../type";
 import { fromBinary, getPuzzleInput, toBinary, invert } from "../../utils";
 
-const wires: ILooseObject = {};
+const rawWires: ILooseObject = {};
+const solvedWires: ILooseObject = {};
 
 function processWire(line: string){
     const [input, output] = line.split(" -> ");
-    wires[output] = input;
+
+    const parsedValue = parseInt(input)
+    if(!isNaN(parsedValue)){ //hooray, we have a raw value!
+        console.log(`Setting wire ${output} to ${parsedValue}`);
+        solvedWires[output] = parsedValue;
+    } else {
+        rawWires[output] = input.split(" ");
+    }
+}
+
+function solve(wire: string, value: number){
+    solvedWires[wire] = value;
+    delete rawWires[wire];
+
+    console.log(`Solved ${wire}`);
+}
+
+function solveWires(){
+    let keys = Object.keys(rawWires);
+    while(keys.length > 0){
+        console.log(keys);
+
+        for(const wire of keys){
+            const parts = rawWires[wire];
+
+            for(let i = 0; i < parts.length; i++){
+                const prev = (i == 0) ? null : parts[i - 1];
+                const part = parts[i];
+                const next = (i == parts.length - 1) ? null : parts[i + 1];
+
+                if(part == "NOT"){
+                    if(!solvedWires.hasOwnProperty(next)){
+                        console.log(`Missing wire '${next}' needed for ${wire}`);
+                        break; //we don't have the wire to solve this yet
+                    }
+
+                    const bits = toBinary(solvedWires[next], 16);
+                    invert(bits);
+                    solve(wire, fromBinary(bits));
+                    break;
+                }
+
+                if(part == 'AND'){
+                    if(!solvedWires.hasOwnProperty(prev) || !solvedWires.hasOwnProperty(next)){
+                        console.log(`Missing either wire '${prev}' or '${next}' needed for ${wire}`);
+                        break; //we don't have the wires to solve this yet
+                    }
+
+                    solve(wire, solvedWires[prev] & solvedWires[next]);
+                    break;
+                }
+
+                if(part == 'OR'){
+                    if(!solvedWires.hasOwnProperty(prev) || !solvedWires.hasOwnProperty(next)){
+                        console.log(`Missing either wire '${prev}' or '${next}' needed for ${wire}`);
+                        break; //we don't have the wires to solve this yet
+                    }
+
+                    solve(wire, solvedWires[prev] | solvedWires[next]);
+                    break;
+                }
+
+                if(part == 'LSHIFT'){
+                    if(!solvedWires.hasOwnProperty(prev)){
+                        console.log(`Missing wire '${prev}' needed for ${wire}`);
+                        break; //we don't have the wire to solve this
+                    }
+
+                    solve(wire, solvedWires[prev] << parseInt(next));
+                    break;
+                }
+
+                if(part == 'RSHIFT'){
+                    if(!solvedWires.hasOwnProperty(prev)){
+                        console.log(`Missing wire '${prev}' needed for ${wire}`);
+                        break; //we don't have the wire to solve this
+                    }
+
+                    solve(wire, solvedWires[prev] >> parseInt(next));
+                    break;
+                }
+            }
+        }
+
+        keys = Object.keys(rawWires);
+    }
 }
 
 const input = getPuzzleInput(__dirname, "example.txt");
 input.forEach(processWire);
 
-console.log(wires);
 
-
-const bits = toBinary(123, 16);
-console.log(bits);
-invert(bits);
-console.log(fromBinary(bits));
+console.log(rawWires);
+solveWires();
+console.log(solvedWires);
