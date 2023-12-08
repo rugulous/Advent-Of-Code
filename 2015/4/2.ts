1fdcb0965f8e610bd5874468ba2eb8ade0617741
Luke Taylor <luke@circleleasing.co.uk>
2023-12-08 13:05:04 +0000

    15/4.2

---
+++ 2015/4/2.ts
@@ -0,0 +1,15 @@
+import { createHash } from "crypto";
+
+const secret = 'bgvyzdsv';
+let num = 0;
+
+while(true){
+    const str = createHash("md5").update(secret + num).digest("hex");
+
+    if(str.startsWith('000000')){
+        console.log(num);
+        break;
+    }
+
+    num++;
+}
