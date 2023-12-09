eb243c5ce69ccc89d5e2d557dc04b0a6b6dc6247
Luke Taylor <luke@circleleasing.co.uk>
2023-12-09 11:55:06 +0000

    9/1.E

--- 2023/9/1.ts
+++ 2023/9/1.ts
@@ -11,11 +11,9 @@ function getDifferenceBetweenSequence(sequence: number[]){
 
 function getNextValueForSequence(sequence: number[]){
     let differences = getDifferenceBetweenSequence(sequence);
-
-    if(arrayHasSameValue(differences)){
-        return sequence[sequence.length - 1] + differences[0];
-    }
+    const numToAdd = arrayHasSameValue(differences) ? differences[0] : getNextValueForSequence(differences);
+    return sequence[sequence.length - 1] + numToAdd;
 }
 
 const input = getPuzzleInput(__dirname, "example.txt").map(line => line.split(" ").map(n => parseInt(n)));
-console.log(getNextValueForSequence(input[0]));
+console.log(input.reduce((acc, val) => acc + getNextValueForSequence(val), 0));
