8da8ac64a92dbdb12f8a2778a0967e4fad06edac
Luke Taylor <luke@circleleasing.co.uk>
2023-12-11 15:58:14 +0000

    11.2

---
+++ 2023/11/2.ts
@@ -0,0 +1,75 @@
+import { arrayHasSameValue, columnHasSameValue, getPuzzleInput } from "../../utils";
+
+const expansion = 999_999;
+
+function expandVertically(universe: string[][]) {
+    const offsets = [];
+    let currOffset = 0;
+
+    for (let i = 0; i < universe.length; i++) {
+        offsets.push(currOffset);
+
+        if (arrayHasSameValue(universe[i], ".")) {
+            currOffset += expansion;
+        }
+    }
+
+    return offsets;
+}
+
+function expandHorizontally(universe: string[][]) {
+    const offets = [];
+    let currOffset = 0;
+
+    for (let i = 0; i < universe[0].length; i++) {
+        offets.push(currOffset);
+
+        if (columnHasSameValue(universe, i, ".")) {
+            currOffset += expansion;
+        }
+    }
+
+    return offets;
+}
+
+function getOffsetCoordinate(y: number, x: number) {
+    return {
+        y: y + offsets.y[y],
+        x: x + offsets.x[x]
+    };
+}
+
+function expandUniverse(universe: string[][]) {
+    const y = expandVertically(universe);
+    const x = expandHorizontally(universe);
+
+    return { x, y };
+}
+
+function locateGalaxies(universe: string[][]) {
+    const galaxies = [];
+
+    for (let y = 0; y < universe.length; y++) {
+        for (let x = 0; x < universe[0].length; x++) {
+            if (universe[y][x] == "#") {
+                galaxies.push(getOffsetCoordinate(y, x));
+            }
+        }
+    }
+
+    return galaxies;
+}
+
+const input = getPuzzleInput(__dirname).map(l => l.split(""));
+const offsets = expandUniverse(input);
+
+let total = 0;
+const galaxies = locateGalaxies(input);
+for (let i = 0; i < galaxies.length; i++) {
+    for (let j = i + 1; j < galaxies.length; j++) {
+        const distance = Math.abs(galaxies[i].x - galaxies[j].x) + Math.abs(galaxies[i].y - galaxies[j].y);
+        total += distance;
+    }
+}
+
+console.log(total);
