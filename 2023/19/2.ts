b04dd037e096e7a3d0d894efed08208ee56dfa08
Luke Taylor <luke@circleleasing.co.uk>
2023-12-19 22:51:25 +0000

    Ranges???

--- 2023/19/2.ts
+++ 2023/19/2.ts
@@ -1,192 +1,149 @@
 import { getPuzzleInput } from "../../utils";
 
-const MIN_VAL = 1;
-const MAX_VAL = 4000;
-
-type Limits = {
-    x: [number, number],
-    m: [number, number],
-    a: [number, number],
-    s: [number, number]
-}
-
-class Branch {
-    identifier: string;
-    field: string;
-    value: number;
-    pass: Branch | string;
-    fail: Branch | string;
-    rawCondition: string;
-
-    constructor(identifier) {
-        this.identifier = identifier;
-    }
-}
-
-function parseInput(input: string[]): {} {
+function parseInput(input: string[]): [{}, any[]]{
+    let dataStart = false;
     const flows = {};
-
-    for (const line of input) {
-        if (line.trim() == "") {
-            break;
-        }
-
-        const [label, branch] = splitWorkflow(line); //processWorkflow(line);
-
-        if (branch.length > 0) {
-            flows[label] = branch;
-        }
-    }
-
-    pruneBranches(flows);
-
-    return flows;
-}
-
-function pruneBranches(workflows: { [key: string]: any[] }) {
-    while (true) {
-        let changesMade = 0;
-        let hasSolution = false;
-
-        for (const key in workflows) {
-            if (!Array.isArray(workflows[key][0])) {
-                continue;
-            }
-
-            for (let i = 0; i < workflows[key].length; i++) {
-                const conditions = workflows[key][i];
-                const target = conditions[conditions.length - 1];
-                if (target == "A") {
-                    hasSolution = true;
-                    continue;
-                }
-
-                if (!workflows[target]) {
-                    workflows[key].splice(i, 1);
-                    changesMade++;
-                } else if (!Array.isArray(workflows[target][0])) {
-                    conditions[conditions.length - 1] = workflows[target];
-                    changesMade++;
-                }
-            }
-
-            if (workflows[key].length == 0) {
-                delete workflows[key];
-                changesMade++;
-            } else if (workflows[key].length == 1 && hasSolution) {
-                workflows[key][0].pop();
-                workflows[key] = workflows[key][0];
-                changesMade++;
-            }
-        }
-
-        if (changesMade == 0) {
-            break;
-        }
-    }
-}
-
-function invert(condition: string) {
-    if (condition.indexOf(">") >= 0) {
-        return condition.replace(">", "<=");
-    }
-
-    return condition.replace("<", ">=");
-}
-
-function splitWorkflow(line: string): [string, any[]] {
-    const [label, body] = line.replace("}", "").split("{");
-    const funcs = body.split(",");
-
-    const options = [];
-    const currPath = [];
-    for (const func of funcs) {
-        if (func.indexOf(":") < 0) {
-            if (func != "R") {
-                options.push([...currPath, func]);
-            }
+    const data = [];
+    
+    for(const line of input){
+        if(line.trim() == ""){
+            dataStart = true;
             continue;
         }
 
-        const [cond, dest] = func.split(":");
-        if (dest != "R") {
-            options.push([...currPath, cond, dest]);
+        if(dataStart){
+            data.push(processData(line));
+        } else {
+            const [label, flow] = processWorkflow(line);
+            flows[label] = flow;
         }
-        currPath.push(invert(cond));
     }
 
-    return [label, options];
+    return [flows, data];
+}
+
+function processWorkflow(line: string): [string, any[]]{
+    const [label, body] = line.replace("}", "").split("{");
+    const funcs = body.split(",");
+    const arr = [];
+
+    for(const fun of funcs){
+        const [expr, result] = fun.split(":");
+
+        const fn = (range: {}) => {
+            if(!result){
+                console.log(`${result}: EARLY EXIT`);
+                return [{result: expr, range}];
+            }
+
+            const [variable, rawNum, lessThan] = extractVariable(expr);
+            const num = parseInt(rawNum);
+
+            const passRange = JSON.parse(JSON.stringify(range));
+            const failRange = JSON.parse(JSON.stringify(range));
+
+            if(lessThan){
+                passRange[variable][1] = Math.min(passRange[variable][1], num - 1);
+                failRange[variable][0] = Math.max(failRange[variable][0], num);
+            } else {
+                failRange[variable][1] = Math.min(failRange[variable][1], num - 1);
+                passRange[variable][0] = Math.max(passRange[variable][0], num);
+            }
+            
+
+            return [{
+                result,
+                range: passRange
+            }, {
+                result: null,
+                range: failRange
+            }];
+        };
+
+        arr.push(fn);
+    }
+
+    return [label, arr];
+}
+
+function extractVariable(test: string): [string, string, boolean]{
+    if(test.indexOf("<") >= 0){
+        return [...test.split("<"), true] as [string, string, boolean];
+    }
+
+    return [...test.split(">"), false] as [string, string, boolean];
+}
+
+function processData(line: string): {x: number, m: number, a: number, s: number}{
+    line = line.replaceAll("=", '":').replaceAll(",", ',"').replace("{", '{"').replace("}", '}'); //make JSON-able
+    return JSON.parse(line);
+}
+
+const approved = []
+const approvedRuns = [];
+function parseRange(currWorkflow, currIndex, range, numRuns = 0){
+    console.log(`Running ${currWorkflow}[${currIndex}]`);
+    const [pass, fail] = workflows[currWorkflow][currIndex]({...range});
+    console.log(pass);
+    console.log(fail);
+    
+    if(pass.result == "A"){
+        console.log(`${currWorkflow}${currIndex} has resulted in ${pass.result}`);
+        approved.push(pass.range);
+    } else if(pass.result == "R"){
+        console.log("REJECTED");
+    } else {
+        parseRange(pass.result, 0, JSON.parse(JSON.stringify(pass.range)), numRuns + 1);
+    }
+
+    if(fail){
+        if(!fail.result){
+            parseRange(currWorkflow, currIndex + 1,  JSON.parse(JSON.stringify(fail.range)), numRuns + 1);
+        }
+    }
 }
 
 const input = getPuzzleInput(__dirname, "example.txt");
-const workflows = parseInput(input);
+const [workflows, items] = parseInput(input);
 
-console.log(workflows);
-
-
+const accepted = [];
 
 
+    let currWorkflow = "in";
+    let currIndex = 0;
+
+    let range = {
+        x: [1, 4000],
+        m: [1, 4000],
+        a: [1, 4000],
+        s: [1, 4000]
+    };
+
+    parseRange(currWorkflow, currIndex, range);
+    console.log(approved.filter(a => a.x[0] != 1 && a.x[1] != 4000).map(a => a.x));
+    console.log(approved.filter(a => a.m[0] != 1 && a.m[1] != 4000).map(a => a.m));
+    console.log(approved.filter(a => a.a[0] != 1 && a.a[1] != 4000).map(a => a.a));
+    console.log(approved.filter(a => a.s[0] != 1 && a.s[1] != 4000).map(a => a.s));
+    
+//     while(true){
+//         const result = workflows[currWorkflow][currIndex](range);
+//         console.log(result);
+//         break;
+
+//         if(result != null){
+//             if(result == "A"){
+//                 //accepted.push(item);
+//                 break;
+//             } else if(result == "R"){
+//                 break;
+//             }
+
+//             currWorkflow = result;
+//             currIndex = 0;
+//         } else {
+//             currIndex++;
+//         }
+//     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function processWorkflow(line: string) {
-    const [label, body] = line.replace("}", "").split("{");
-    const funcs = body.split(",");
-
-    let firstBranch = null;
-    let lastBranch = null;
-
-    for (let i = 0; i < funcs.length; i++) {
-        const [cond, dest] = funcs[i].split(":");
-
-        if (!dest) {
-            lastBranch.fail = cond;
-            break;
-        }
-
-        const name = (i == 0) ? label : `${label}-${i}`;
-        const [field, value] = extractCondition(cond);
-
-        const newBranch = new Branch(name);
-        newBranch.field = field;
-        newBranch.value = parseInt(value);
-        newBranch.pass = dest;
-        newBranch.rawCondition = cond;
-
-        if (lastBranch) {
-            lastBranch.fail = newBranch;
-        } else {
-            firstBranch = newBranch;
-        }
-
-        lastBranch = newBranch;
-    }
-
-    return [label, firstBranch];
-}
-
-function extractCondition(condition: string) {
-    if (condition.indexOf(">") > -1) {
-        return condition.split(">");
-    } else if (condition.indexOf("<") > -1) {
-        return condition.split("<");
-    }
-
-    return condition;
-}
-*/
+// console.log(accepted.reduce((acc, val) => acc + val.x + val.m + val.a + val.s, 0));
