8261394090655c51015a1ca6caf02cf0c563902f
Luke Taylor <luke@circleleasing.co.uk>
2024-12-04 12:31:22 +0000

    2015/13.1

--- utils.ts
+++ utils.ts
@@ -259,3 +259,23 @@ export const mirroredMoves: Record<Direction, Direction> = {
 export const allDirs: Direction[] = ["UP", "DOWN", "LEFT", "RIGHT"];
 
 export const betweenInclusive = (x: number, min: number, max: number) => x >= min && x <= max;
+
+export function getPermutations<T>(inputArr: T[]) {
+    let result: T[][] = [];
+
+    const permute = (arr: T[], m: T[] = []) => {
+        if (arr.length === 0) {
+            result.push(m)
+        } else {
+            for (let i = 0; i < arr.length; i++) {
+                let curr = arr.slice();
+                let next = curr.splice(i, 1);
+                permute(curr.slice(), m.concat(next))
+            }
+        }
+    }
+
+    permute(inputArr)
+
+    return result;
+}
