2e7dce63f365fef0eae62a0b96796296f0901929
Luke Taylor <luke@circleleasing.co.uk>
2024-12-04 12:33:14 +0000

    2015/13.2

---
+++ 2015/13/2.ts
@@ -0,0 +1,45 @@
+import { getPermutations, getPuzzleInput } from "../../utils";
+
+function parseInput(input: string[]){
+    const regex = /(.*) would (gain|lose) (\d*) happiness units by sitting next to (.*)./
+    const people: {[key: string]: {}} = {'Me': {}};
+
+    for(const line of input){
+        const match = regex.exec(line);
+        if(!match){
+            continue;
+        }
+
+        const [_, person, action, value, neighbour] = match;
+
+        if(!people.hasOwnProperty(person)){
+            people[person] = {'Me': 0};
+        }
+
+        if(!people.Me.hasOwnProperty(person)){
+            people.Me[person] = 0;
+        }
+
+        people[person][neighbour] = (action == "gain" ? 1 : -1) * parseInt(value);
+    }
+
+    return people;
+}
+
+const people = parseInput(getPuzzleInput(__dirname));
+
+const permutations = getPermutations(Object.keys(people)).map(arr => {
+    let score = 0;
+    for(let i = 0; i < arr.length; i++){
+        const left = arr[i];
+        const right = arr[(i + 1) % arr.length]; //mod allows circular arrangement
+        score += people[left][right] + people[right][left];
+    }
+    
+    return {
+        score,
+        arr
+    }
+}).sort((a,b) => b.score - a.score);
+
+console.log(permutations[0]);
