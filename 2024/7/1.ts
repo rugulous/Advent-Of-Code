bd4629c650775293255c659c41a5ad852b24121c
Luke Taylor <luke@circleleasing.co.uk>
2024-12-07 14:13:10 +0000

    7.1

---
+++ 2024/7/1.ts
@@ -0,0 +1,29 @@
+import { getPuzzleInput } from "../../utils";
+
+function canMeetTarget(numbers: number[], target: number, current: number = 0, index: number = 0): boolean {
+    if(current > target){
+        return false; //BUST
+    }
+
+    if (index == numbers.length) {
+        return current == target;
+    }
+
+    const nextNum = numbers[index];
+    const canAdd = canMeetTarget(numbers, target, current + nextNum, index + 1);
+    const canMultiply = canMeetTarget(numbers, target, current * nextNum, index + 1);
+
+    return canAdd || canMultiply;
+}
+
+const total = getPuzzleInput(__dirname).map(line => {
+    const [target, remaining] = line.split(":");
+    const values = remaining.trim().split(" ");
+    return {
+        target: parseInt(target),
+        values: values.map(x => parseInt(x)),
+    }
+}).filter(eq => canMeetTarget(eq.values, eq.target))
+.reduce((acc, val) => acc + val.target, 0);
+
+console.log(total);
