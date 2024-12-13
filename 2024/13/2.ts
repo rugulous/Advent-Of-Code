666e8a845e54e75b96925c539ad1fe8eebc881ab
Luke Taylor <luke@circleleasing.co.uk>
2024-12-13 14:27:25 +0000

    13.2

---
+++ 2024/13/2.ts
@@ -0,0 +1,27 @@
+import { getPuzzleInput } from "../../utils";
+
+const btnRegex = /X\+(\d*), Y\+(\d*)/
+const prizeRegex = /X=(\d*), Y=(\d*)/
+
+const input = getPuzzleInput(__dirname);
+
+let total = 0;
+for (let i = 0; i < input.length; i += 4) {
+    const [, _ax, _ay] = btnRegex.exec(input[i]);
+    const [, _bx, _by] = btnRegex.exec(input[i + 1]);
+
+    const [, _x, _y] = prizeRegex.exec(input[i + 2]);
+
+    //thank you Reddit - https://www.reddit.com/r/adventofcode/comments/1hd7irq/2024_day_13_an_explanation_of_the_mathematics/
+    const [ax, ay, bx, by] = [_ax, _ay, _bx, _by].map(x => parseInt(x));
+    const [x, y] = [_x, _y].map(x => parseInt(x) + 10000000000000);
+
+    const a = ((x * by) - (y * bx)) / ((ax * by) - (ay * bx));
+    const b = ((ax * y) - (ay * x)) / ((ax * by) - (ay * bx));
+
+    if(a % 1 == 0 && b % 1 == 0){
+        total += (a * 3) + b;
+    }
+}
+
+console.log(total);
