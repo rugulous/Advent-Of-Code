f2690a45069c02ab9234733e2503063e3224d62f
Luke Taylor <luke@circleleasing.co.uk>
2024-12-18 12:47:04 +0000

    18.1 (switched to BFS)

--- 2024/18/1.ts
+++ 2024/18/1.ts
@@ -1,51 +1,55 @@
-import { getPuzzleInput, grid, isOutOfBounds } from "../../utils";
+import { Coordinate } from "../../type";
+import { allDirs, getPuzzleInput, grid, isOutOfBounds, move } from "../../utils";
 
-const TARGET_SIZE = 7; //grid goes from 0-6, so 7 total
-const NUM_TO_PROCESS = 12;
+class NodePos {
+    coordinate: Coordinate;
+    distance: number;
+
+    constructor(coord: Coordinate, dist: number){
+        this.coordinate = coord;
+        this.distance = dist;
+    }
+}
+
+const TARGET_SIZE = 71; //grid goes from 0-70, so 71 total
+const NUM_TO_PROCESS = 1024;
 
 const destination = {
     x: TARGET_SIZE - 1,
     y: TARGET_SIZE - 1
 }
 
-const moves = [
-    [0, 1],
-    [0, -1],
-    [1, 0],
-    [-1, 0]
-];
+function findShortestPath(startPos: Coordinate){
+    const queue = [new NodePos(startPos, 0)];
 
-function isSafe(x: number, y: number){
-    return !isOutOfBounds({x, y}, map) && map[y][x] && !visited[y][x];
-}
+    while(queue.length > 0){
+        const curr = queue.shift();
 
-let minDist = Infinity;
+        if(curr.coordinate.x == destination.x && curr.coordinate.y == destination.y){
+            return curr.distance;
+        }
 
-function findShortestPath(x: number, y: number, currDist: number){
-    if(x == destination.x && y == destination.y){
-        minDist = Math.min(minDist, currDist);
-        return;
-    }
+        for(const dir of allDirs){
+            const pos = move[dir](curr.coordinate.x, curr.coordinate.y);
 
-    visited[y][x] = true;
-
-    for(const [moveX, moveY] of moves){
-        if(isSafe(x + moveX, y + moveY)){
-            findShortestPath(x + moveX, y + moveY, currDist + 1);
+            if(!isOutOfBounds(pos, map) && map[pos.y][pos.x] && !visited[pos.y][pos.x]){
+                visited[pos.y][pos.x] = true;
+                queue.push(new NodePos(pos, curr.distance + 1));
+            }
         }
     }
 
-    visited[y][x] = false;
+    return -1;
 }
 
 const map = grid(TARGET_SIZE, true, TARGET_SIZE);
 const visited = grid(TARGET_SIZE, false, TARGET_SIZE);
 
-getPuzzleInput(__dirname, "example.txt").slice(0, NUM_TO_PROCESS).forEach(line => {
+getPuzzleInput(__dirname).slice(0, NUM_TO_PROCESS).forEach(line => {
     const [x, y] = line.split(",").map(x => parseInt(x))
     map[y][x] = false;
 });
 
 
-findShortestPath(0, 0, 0); //start at top left corner and work to bottom right
+const minDist = findShortestPath({x: 0, y: 0}); //start at top left corner and work to bottom right
 console.log(minDist);
