2df5f4f81d65d8ed650b52210ebf8434508c9496
Luke Taylor <luke@circleleasing.co.uk>
2024-12-23 12:01:58 +0000

    Neaten up somewhat

--- 2024/21/1.ts
+++ 2024/21/1.ts
@@ -8,9 +8,19 @@ const movesToDirs = {
     "v": "DOWN"
 };
 
+//left is furthest from A (v<<)
+//then down (v<)
+//up (<) and right (v) are equidistant
+const movementWeights = {
+    "<": 3,
+    "v": 2,
+    "^": 1,
+    ">": 1
+};
+
 const codes = getPuzzleInput(__dirname);
 
-function areCoordsEqual(p1: Coordinate, p2: Coordinate){
+function areCoordsEqual(p1: Coordinate, p2: Coordinate) {
     return p1.x == p2.x && p1.y == p2.y;
 }
 
@@ -24,62 +34,48 @@ function getPosition(keypad: (string | null)[], button: string) {
 
 function getMovesTo(startPos: Coordinate, { x, y }: Coordinate, toAvoid: Coordinate, prevMove = null) {
     let directions: string[] = [];
-    const currPos = {...startPos};
+    const currPos = { ...startPos };
 
-    while (!areCoordsEqual(currPos, {x,y})) {
-        if (currPos.x < x && !areCoordsEqual(toAvoid, {x: currPos.x + 1, y: currPos.y})) {
+    while (!areCoordsEqual(currPos, { x, y })) {
+        if (currPos.x < x && !areCoordsEqual(toAvoid, { x: currPos.x + 1, y: currPos.y })) {
             directions.push(">");
             currPos.x++;
-        } else if (currPos.x > x && !areCoordsEqual(toAvoid, {x: currPos.x - 1, y: currPos.y})) {
+        } else if (currPos.x > x && !areCoordsEqual(toAvoid, { x: currPos.x - 1, y: currPos.y })) {
             directions.push("<")
             currPos.x--;
-        } else if (currPos.y < y && !areCoordsEqual(toAvoid, {x: currPos.x, y: currPos.y + 1})) {
+        } else if (currPos.y < y && !areCoordsEqual(toAvoid, { x: currPos.x, y: currPos.y + 1 })) {
             directions.push("v");
             currPos.y++;
-        } else if (currPos.y > y && !areCoordsEqual(toAvoid, {x: currPos.x, y: currPos.y - 1})) {
+        } else if (currPos.y > y && !areCoordsEqual(toAvoid, { x: currPos.x, y: currPos.y - 1 })) {
             directions.push("^");
             currPos.y--;
         } else {
-            console.log("SKIPPED!");
-            console.log(currPos);
-            console.log(toAvoid);
-            console.log({x, y});
-            throw '';
+            throw new Error("We are stuck and unable to move!");
         }
     }
 
     directions = tryToFixDirections(directions, startPos, toAvoid, prevMove);
 
+    //need to overwrite this too as behaviour prior to refactor was expecting modification in place
     startPos.x = x;
     startPos.y = y;
 
     return directions;
 }
 
-function tryToFixDirections(directions: string[], startPos: Coordinate, toAvoid: Coordinate, prevMove: string | null){
+function tryToFixDirections(directions: string[], startPos: Coordinate, toAvoid: Coordinate, prevMove: string | null) {
     const hDirs = ['<', '>'];
     const yDirs = ['^', 'v'];
 
-
-    //left is furthest from A (v<<)
-    //then down (v<)
-    //up (<) and right (v) are equidistant
-    const weights = {
-        "<": 3,
-        "v": 2,
-        "^": 1,
-        ">": 1
-    };
-
-    if(directions.some(x => hDirs.includes(x)) && directions.some(y => yDirs.includes(y))){
-        //we're moving horizontally AND vertically, so there must be some combination that avoids the null square!
-        const targetDirs = directions.toSorted((a, b) => a == prevMove ? 1 : weights[b] - weights[a]);     
-        if(doMovesStillWork(targetDirs, {...startPos}, toAvoid)){
+    //we're moving horizontally AND vertically, so there must be some combination that avoids the null square!
+    if (directions.some(x => hDirs.includes(x)) && directions.some(y => yDirs.includes(y))) {
+        const targetDirs = directions.toSorted((a, b) => a == prevMove ? 1 : movementWeights[b] - movementWeights[a]);
+        if (doMovesStillWork(targetDirs, { ...startPos }, toAvoid)) {
             return targetDirs
         }
-        
-        targetDirs.sort((a, b) => a == prevMove ? 1 : weights[a] - weights[b])
-        if(doMovesStillWork(targetDirs, {...startPos}, toAvoid)){
+
+        targetDirs.sort((a, b) => a == prevMove ? 1 : movementWeights[a] - movementWeights[b])
+        if (doMovesStillWork(targetDirs, { ...startPos }, toAvoid)) {
             return targetDirs
         }
     }
@@ -87,16 +83,16 @@ function tryToFixDirections(directions: string[], startPos: Coordinate, toAvoid:
     return directions;
 }
 
-function doMovesStillWork(moves: string[], pos: Coordinate, toAvoid: Coordinate){
-    for(const dir of moves){
+function doMovesStillWork(moves: string[], pos: Coordinate, toAvoid: Coordinate) {
+    for (const dir of moves) {
         const targetMove = movesToDirs[dir];
-        if(!targetMove){
+        if (!targetMove) {
             continue;
         }
 
         pos = move[targetMove](pos.x, pos.y);
 
-        if(areCoordsEqual(pos, toAvoid)){
+        if (areCoordsEqual(pos, toAvoid)) {
             return false;
         }
     }
@@ -105,21 +101,18 @@ function doMovesStillWork(moves: string[], pos: Coordinate, toAvoid: Coordinate)
 }
 
 const dirKeypad = [null, '^', 'A', '<', 'v', '>'];
-const dirGap = {x: 0, y: 0};
+const dirGap = { x: 0, y: 0 };
 
 const numKeypad = ['7', '8', '9', '4', '5', '6', '1', '2', '3', null, '0', 'A'];
-const numGap = {x:0, y: 3};
+const numGap = { x: 0, y: 3 };
 
 let codeBot: Coordinate = { x: 2, y: 3 };
 let dirBot: Coordinate = { x: 2, y: 0 };
-let thirdBot: Coordinate = { x: 2, y: 0 };
 
 let total = 0;
 
 for (const code of codes) {
-    console.log(code);
-    
-    const directions: string[] = [];
+    let directions: string[] = [];
     let numStr = "";
 
     for (const char of code) {
@@ -128,46 +121,26 @@ for (const code of codes) {
         }
 
         const target = getPosition(numKeypad, char);
-
         directions.push(...getMovesTo(codeBot, target, numGap));
-
         directions.push("A");
     }
 
-    console.log(directions);
+    for(let i = 0; i < 2; i++){
+        let moves = []; //moves is just intermediate directions (great naming i know...)
+        let prevDir = null;
 
-    const actualDirections: string[] = [];
-    let prevDir = null;
+        for (const dir of directions) {
+            const target = getPosition(dirKeypad, dir);
+    
+            moves.push(...getMovesTo(dirBot, target, dirGap, prevDir), "A");
+            prevDir = moves[moves.length - 2];
+        }
 
-    for (const dir of directions) {
-        const target = getPosition(dirKeypad, dir);
-
-        actualDirections.push(...getMovesTo(dirBot, target, dirGap, prevDir));
-        prevDir = actualDirections[actualDirections.length - 1];
-        actualDirections.push("A");
+        directions = moves;
     }
-
-    console.log(actualDirections);
-
-    const myDirections: string[] = [];
-    prevDir = null;
-
-    for (const dir of actualDirections) {
-        const target = getPosition(dirKeypad, dir);
-
-        myDirections.push(...getMovesTo(thirdBot, target, dirGap, prevDir));
-        prevDir = myDirections[myDirections.length - 1];
-        myDirections.push("A");
-    }
-
-    console.log(myDirections);
 
     const num = parseInt(numStr);
-    total += (num * myDirections.length);
-
-    console.log(`${code} = ${num}`);
-    console.log(`${myDirections.length} total directions`);
-    console.log("Score of ", num * myDirections.length);
+    total += (num * directions.length);
 }
 
 console.log(total);
