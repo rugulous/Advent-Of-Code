304b651fbae78591089a40a2be478425d0f4bb70
Luke Taylor <luke@circleleasing.co.uk>
2024-12-23 14:38:55 +0000

    23.1

---
+++ 2024/23/1.ts
@@ -0,0 +1,39 @@
+import { getPuzzleInput } from "../../utils";
+
+const input = getPuzzleInput(__dirname);
+const connections = {};
+
+for(const line of input){
+    const [left, right] = line.split("-");
+
+    if(!connections.hasOwnProperty(left)){
+        connections[left] = [];
+    }
+
+    if(!connections.hasOwnProperty(right)){
+        connections[right] = [];
+    }
+
+    connections[left].push(right);
+    connections[right].push(left);
+}
+
+const groups = new Set<string>();
+
+for(const key of Object.keys(connections)){
+    for(const node of connections[key]){
+        
+        for(const leftNode of connections[key]){
+            for(const rightNode of connections[node]){
+                if(leftNode == rightNode){
+                    const group = [key, node, leftNode].toSorted().join(",");
+                    groups.add(group);
+                }
+            }
+        }
+
+    }
+}
+
+const matches = [...groups].reduce((acc, val) => acc + (val.split(",").some(x => x.startsWith("t")) ? 1 : 0), 0);
+console.log(matches);
